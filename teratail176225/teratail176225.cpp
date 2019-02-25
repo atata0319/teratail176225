efb2a575725d7294f5c4e3f32246b560e57e7c0a
ichiro <atata0319@hotmail.com>
2019-02-26 02:55:20 +0900

    対応版

--- teratail176225/teratail176225.cpp
+++ teratail176225/teratail176225.cpp
@@ -1,15 +1,20 @@
 #define UNICODE
 
 #include <Windows.h>
+#include <Windowsx.h> // GET_X_LPARAM と GET_Y_LPARAM 用
 
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WindowMouseProc(HWND, UINT, WPARAM, LPARAM); // 透明ウィンドウ用プロシージャ
 LRESULT CALLBACK WindowDestroyProc(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE hGlobalInstance;
 
 const wchar_t CLASS_NAME[] = L"CLASS NAME";
+const wchar_t MOUSE_CLASS_NAME[] = L"MOUSE CLASS NAME";
 const wchar_t DESTROY_CLASS_NAME[] = L"DESTROY CONTROL CLASS NAME";
 
 HWND mhwnd;
+HWND mmouse;
+bool mshowdialog = false;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR nCmdLine, int nCmdShow) {
 	UNREFERENCED_PARAMETER(hPrevInstance);
@@ -25,18 +30,37 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR nCmdLin
 
 	RegisterClassEx(&wc);
 
-	HWND hwnd = CreateWindowEx(
+	mhwnd = CreateWindowEx(
 		WS_EX_LAYERED, CLASS_NAME, NULL, WS_POPUP | WS_BORDER,
 		100, 100, 960, 540,
 		NULL, NULL, hInstance, NULL
 	);
 
-	mhwnd = hwnd;
+	SetLayeredWindowAttributes(mhwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
 
-	SetLayeredWindowAttributes(hwnd, 0x0, 0, LWA_COLORKEY);
+	WNDCLASSEX wc2 = {
+		sizeof(WNDCLASSEX), CS_VREDRAW | CS_HREDRAW, WindowMouseProc,
+		0, 0, hInstance,
+		NULL, (HCURSOR)LoadCursor(NULL,IDC_ARROW), (HBRUSH)GetStockObject(WHITE_BRUSH),
+		NULL, MOUSE_CLASS_NAME, NULL
+	};
 
-	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);
+	RegisterClassEx(&wc2);
+
+	mmouse = CreateWindowEx(
+		WS_EX_LAYERED | WS_EX_NOACTIVATE, MOUSE_CLASS_NAME, NULL, WS_POPUP,
+		100, 100, 960, 540,
+		NULL, NULL, hInstance, NULL
+	);
+
+	// 以下、不要でした。
+	// SetLayeredWindowAttributes(mmouse, 0, 1, LWA_ALPHA);
+
+	ShowWindow(mhwnd, nCmdShow);
+	SetWindowPos(mmouse, mhwnd, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
+
+	UpdateWindow(mmouse);
+	UpdateWindow(mhwnd);
 
 	MSG msg = {};
 
@@ -70,10 +94,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 	}
 	break;
 
-	case WM_LBUTTONDOWN:
-		PostMessage(hwnd, WM_NCLBUTTONDOWN, HTCAPTION, NULL);
-		break;
-
 	case WM_PAINT:
 	{
 		PAINTSTRUCT ps;
@@ -86,6 +106,35 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 	}
 	break;
 
+	case WM_LBUTTONDOWN:
+		PostMessage(mhwnd, WM_NCLBUTTONDOWN, HTCAPTION, NULL);
+		return 0;
+
+	case WM_APP + WM_LBUTTONDOWN - WM_MOUSEFIRST:
+	{
+		if (mshowdialog) {
+			FlashWindow(hwnd, TRUE);
+			MessageBeep(MB_OK);
+			break;
+		}
+		mshowdialog = true;
+		if (MessageBox(hwnd, L"Quit?", L"WM_LBUTTONDOWN", MB_OKCANCEL) == IDOK) {
+			DestroyWindow(mmouse);
+			DestroyWindow(mhwnd);
+		}
+		mshowdialog = false;
+	}
+	return 0;
+
+	case WM_MOVE:
+	case WM_SIZE:
+	{
+		RECT rc;
+		GetWindowRect(mhwnd, &rc);
+		SetWindowPos(mmouse, mhwnd, rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top, SWP_NOACTIVATE);
+	}
+	break;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
@@ -94,10 +143,31 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
+LRESULT CALLBACK WindowMouseProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg >= WM_MOUSEFIRST && uMsg <= WM_MOUSELAST)
+	{
+		// マウス位置をメインウィンドウのクライアント座標に変換して送信。
+		POINT point = { GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam) };
+		MapWindowPoints(hwnd, mhwnd, &point, 1);
+		return SendMessage(mhwnd, WM_APP + uMsg - WM_MOUSEFIRST, wParam, MAKELPARAM(point.x, point.y));
+	}
+	else if (uMsg == WM_MOUSEACTIVATE)
+	{
+		// WS_EX_NOACTIVATE だけでは ACTIVATE してしまうので対策している。
+		return MA_NOACTIVATE;
+	}
+	else
+	{
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+}
+
 LRESULT CALLBACK WindowDestroyProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
 	case WM_LBUTTONUP:
-		SendMessage(mhwnd, WM_DESTROY, 0, 0);
+		DestroyWindow(mmouse);
+		DestroyWindow(mhwnd);
 		return 0;
 
 	case WM_PAINT:
